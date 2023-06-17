40e000b0f6e03a53a549bd4420f533a0e215e95d
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-17 17:12:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  const combinations = [];
+
+  // Helper function to generate combinations recursively
+  function generateCombinations(currentCombination, remainingDigits) {
+    // Base case: All digits have been processed
+    if (remainingDigits.length === 0) {
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const currentDigit = remainingDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    // Iterate through each letter of the current digit
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      generateCombinations(
+        currentCombination + letter,
+        remainingDigits.slice(1) // Slice off the processed digit
+      );
+    }
+  }
+
+  if (digits.length > 0) {
+    generateCombinations('', digits);
+  }
+
+  return combinations;
+}
+	
 }
 
 module.exports = letterCombinations;
